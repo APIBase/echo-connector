8a28e648df1727dea27537627d1bbac9b6429ce9
Freehaha <freehaha@gmail.com>
2017-07-17 17:42:38 +1000

    implement cvs to json connector

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-var APIBase = require("apibase-client");
-var client = new APIBase({
-    baseUrl: 'http://localhost:3000'
-});
+var csvjson = require('csvjson');
 
-
-var GET_SQL = 'aa746210-294a-4e57-bff0-4ba05d948645';
 module.exports = function(source, resourceType, cb) {
-  client.invokeOperation(GET_SQL, source, null, null, function(err, result) {
-    try {
-      var table = JSON.parse(result.body);
-      cb(err, table);
-    } catch (e) {
-      cb(e);
-    }
-  });
+  var data = new Buffer(source.data, 'base64').toString('utf-8');
+  var json = csvjson.toObject(data, source.options);
+  cb(null, json);
 };
--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 var connector = require('./index.js');
 
+var TARGET_RESOURCE_TYPE = 'cvs';
+var CSV = "sr,name,age,gender\n" +
+  "1,rocky,33,male\n" +
+  "2,jacky,22,male\n" +
+  "3,suzy,21,female\n";
+CSV = new Buffer(CSV).toString('base64');
+
 var SOURCE = {
-    "key": "...",
-    "param": "..."
+  data: CSV,
+  options: {
+    delimiter: ',',
+    quote: '""',
+  },
  };
- var TARGET_RESOURCE_TYPE = "gtable";
 
 connector(SOURCE, TARGET_RESOURCE_TYPE, function(err, result) {
   if (err) {
