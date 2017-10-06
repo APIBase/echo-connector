bb57b8462c0845de27120632f459654899492df2
Freehaha <freehaha@gmail.com>
2017-10-06 16:59:37 +1100

    stubs

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var csvjson = require('csvjson');
-
 module.exports = function(source, resourceType, cb) {
-  var data = new Buffer(source.data, 'base64').toString('utf-8');
-  var json = csvjson.toObject(data, source.options);
-  cb(null, json);
+  cb(null, source);
 };
--- test.js
+++ test.js
@@ -1,20 +1,9 @@
 var connector = require('./index.js');
-
-var TARGET_RESOURCE_TYPE = 'cvs';
-var CSV = "sr,name,age,gender\n" +
-  "1,rocky,33,male\n" +
-  "2,jacky,22,male\n" +
-  "3,suzy,21,female\n";
-CSV = new Buffer(CSV).toString('base64');
-
+var TARGET_RESOURCE_TYPE = "*";
 var SOURCE = {
-  data: CSV,
-  options: {
-    delimiter: ',',
-    quote: '""',
-  },
- };
-
+  "name": "test obj",
+  "data": 123
+};
 connector(SOURCE, TARGET_RESOURCE_TYPE, function(err, result) {
   if (err) {
     console.error(err);
